8d96319dca0b70abdd58d561607b31148ffaeda8
Patrick Lewis <pat@lo5t.com>
2022-08-05 22:57:50 -0700

    update

--- remove-old-wordpress-backups/remove-wordpress-backups.sh
+++ remove-old-wordpress-backups/remove-wordpress-backups.sh
@@ -7,9 +7,10 @@ set -euo pipefail
 DAYS=${DAYS:-'365'}
 PATH="/var/www/vhosts/*/wordpress-backups"
 FIND="/bin/find"
+RM="/bin/rm"
 
 remove_wordpress_backups () {
-    $FIND $PATH -type f -mtime +$DAYS -exec echo rm -f {} +
+    $FIND $PATH -type f -mtime +$DAYS -exec $RM -f {} +
 }
 
 remove_wordpress_backups
