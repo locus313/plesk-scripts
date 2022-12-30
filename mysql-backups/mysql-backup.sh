87b9472460f96a53a8f8b71596948a0d19c74a3f
Patrick Lewis <pat@lo5t.com>
2022-12-29 22:31:21 -0800

    updates

--- mysql-backups/mysql-backup.sh
+++ mysql-backups/mysql-backup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
+HOME=/backup/mysql/
+export HOME
 FOLDER=/backup/mysql/data
 PID=/backup/mysql/mysql.pid
 
 /bin/mkdir -p "$FOLDER"
+rm -Rf "$FOLDER/*"
 
 if [ -f $PID ]
 then
@@ -18,34 +20,24 @@ echo $$ > $PID
 
 umask 077
 
-HOME=/backup/mysql/
-export HOME
-
-IFS='
-'
+IFS=''
 /usr/sbin/plesk db -e "show databases" | grep -v -E "^Database|information_schema|performance_schema|phpmyadmin" > $FOLDER/dbs.txt
 
 if [ "x$?" != "x0" ]; then
-
   echo cannot connect to MySQL
   rm -f $PID
   exit 1
-
 fi
 
 echo "Dumping mysql: "
 
 for i in `cat $FOLDER/dbs.txt`;
 do
-
-        rm -Rf "$FOLDER/$n/*"
-
+        echo "$i"
         /usr/sbin/plesk db dump "$i" > $FOLDER/"$i".sql
         
         if [ "x$?" != "x0" ]; then
-
                 echo "Dump fialed for db: $i"
-
         fi
 
 done
