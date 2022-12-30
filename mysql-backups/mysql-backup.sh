814ad8c7111a2f84b232ab3bc0df86c58d70f79e
Patrick Lewis <pat@lo5t.com>
2022-12-29 23:14:19 -0800

    initial mysql-backup.sh script (#4)
    * initial script
    
    * updates
    
    * update
    
    * updates
    
    * update

---
+++ mysql-backups/mysql-backup.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+FOLDER=/backup/mysql/data
+PID=/backup/mysql/mysql.pid
+
+/bin/mkdir -p "$FOLDER"
+
+if [ -f $PID ]
+then
+
+KILL=`cat $PID`
+
+echo "Another instance of script is run trying to kill:" `kill -9 $KILL`
+
+fi
+
+echo $$ > $PID
+
+umask 077
+
+HOME=/backup/mysql/
+export HOME
+
+IFS='
+'
+/usr/sbin/plesk db -e "show databases" | grep -v -E "^Database|information_schema|performance_schema|phpmyadmin" > $FOLDER/dbs.txt
+
+if [ "x$?" != "x0" ]; then
+
+  echo cannot connect to MySQL
+  rm -f $PID
+  exit 1
+
+fi
+
+echo "Dumping mysql: "
+
+for i in `cat $FOLDER/dbs.txt`;
+do
+        /bin/rm -Rf $FOLDER/"$i".sql
+        /usr/sbin/plesk db dump "$i" > $FOLDER/"$i".sql
+        
+        if [ "x$?" != "x0" ]; then
+
+                echo "Dump fialed for db: $i"
+
+        fi
+
+done
+
+echo "mysql backup done"
+
+rm -f $PID
+
+exit 0
